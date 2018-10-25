14769cdca96cc4b05a584954a1cac8cd5fba19a1
Muslim Al-Ali <musl0074@edu.eal.dk>
2018-10-25 15:56:59 +0200

    Implementing ShowScore Method.

--- Udleveret DragonsLair/DragonsLair/Controller.cs
+++ Udleveret DragonsLair/DragonsLair/Controller.cs
@@ -8,14 +8,43 @@ namespace DragonsLair
     public class Controller
     {
         private TournamentRepo tournamentRepository = new TournamentRepo();
-
+        private Round round = new Round();
+        public List<Team> ShowWinner = new List<Team>();
         public void ShowScore(string tournamentName)
         {
-            /*
-             * TODO: Calculate for each team how many times they have won
-             * Sort based on number of matches won (descending)
-             */
-            Console.WriteLine("Implement this method!");
+            Tournament t = tournamentRepository.GetTournament(tournamentName);
+            Team[] teams = t.GetTeams().ToArray();
+            int[] scores = new int[teams.Length];       
+
+            for (int i = 0; i < t.GetNumberOfRounds(); i++)
+            {
+                Round currentRound = t.GetRound(i);
+                List<Team> winningTeams = currentRound.GetWinningTeams();
+                for (int teami = 0; teami < teams.Length; teami++)
+                {
+                    for (int winningTeami = 0; winningTeami < winningTeams.Count; winningTeami++)
+                    {
+                        if (teams[teami].Name == winningTeams[winningTeami].Name)
+                        {
+                            scores[teami]++;
+                        }
+                    }
+                }
+            }
+
+            for (int number = scores.Max(); number >= 0; number--)
+            {
+                for (int i = 0; i < teams.Length; i++)
+                {
+                    if (scores[i] == number)
+                    {
+                        Console.WriteLine("Team: " + teams[i].Name + " Has: " + scores[i] + " Points");
+                    }
+                }
+            }
+
+           
+            Console.ReadLine();
         }
 
         public void ScheduleNewRound(string tournamentName, bool printNewMatches = true)
